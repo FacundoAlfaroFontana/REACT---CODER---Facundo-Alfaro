da964b76212c4fac364f812f97493abbc88b783f
Facundo Alfaro <cfacundoalfarof9@gmail.com>
2024-11-04 01:05:54 -0300

    Primera Pre-Entrega

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react";
+import { BsBackpack3 } from "react-icons/bs";
+
+const CartWidget = () => {
+
+    return(
+        <div style={{display: `Flex`, marginRight: "20px", alignItems: `center`, width: `40px`, justifyContent: `space-between`}}>
+            <BsBackpack3 size={30}/>
+            1
+        </div>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+const ItemListContainer = ({greeting}) => {
+    return (
+        <div style={{fontSize: `2rem`, fontWeight: "bold", textAlign: "center"}}>{greeting}</div>
+    );
+};
+
+export default ItemListContainer;
